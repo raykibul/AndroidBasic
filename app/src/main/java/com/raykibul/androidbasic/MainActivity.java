7673ebfb338a347f01c81086cd1e3d355a8725f4
Rakibul islam <33988342+raykibul@users.noreply.github.com>
2020-07-04 23:16:35 +0600

    Video 3: Toast Added

--- app/src/main/java/com/raykibul/androidbasic/MainActivity.java
+++ app/src/main/java/com/raykibul/androidbasic/MainActivity.java
@@ -5,10 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button buttonTutorial;
+    Button buttonTutorial,toastButton;
 
 
     @Override
@@ -16,13 +17,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         buttonTutorial=findViewById(R.id.button_tutorial);
+        toastButton =findViewById(R.id.toastButton);
 
         buttonTutorial.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 buttonTutorial.setText("Mutton");
-                
+
+            }
+        });
+
+        toastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this,"Toast Button clicked",Toast.LENGTH_SHORT).show();
             }
         });
 
