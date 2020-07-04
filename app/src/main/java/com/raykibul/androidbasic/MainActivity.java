18f47ef10b622fd029c7988c434d48dd2e0c6876
Rakibul islam <33988342+raykibul@users.noreply.github.com>
2020-07-04 23:10:12 +0600

    Video 1: Button added

--- app/src/main/java/com/raykibul/androidbasic/MainActivity.java
+++ app/src/main/java/com/raykibul/androidbasic/MainActivity.java
@@ -3,12 +3,33 @@ package com.raykibul.androidbasic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonTutorial;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonTutorial=findViewById(R.id.button_tutorial);
+
+        buttonTutorial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                buttonTutorial.setText("Mutton");
+                
+            }
+        });
+
+
+
+
+
+
     }
 }
